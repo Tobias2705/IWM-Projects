13eaaa8bc6ebfc317e46465b64f4686b44068566
Grabosk1 <sebastia.grabowski@student.put.poznan.pl>
2022-03-14 23:10:28 +0100

    normalization

---
+++ Tomograph/Normalization.py
@@ -0,0 +1,13 @@
+from skimage.transform import resize
+import numpy as np
+
+
+def normalization(image, frame_size=1024, img_size=624):
+    frame = np.zeros([frame_size, frame_size])
+    img = np.copy(image)
+    img = resize(img, [img_size, img_size])
+    step = int((frame_size - img_size) / 2)
+    for i in range(img_size):
+        for j in range(img_size):
+            frame[i+step][j+step] = img[i][j]
+    return frame
